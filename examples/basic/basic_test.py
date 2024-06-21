e146cb07b9d9a5d9bd4d93660b261eb31b9f6d71
Paul Wankadia <junyer@google.com>
2024-06-21 17:26:07 +0000

    Bump versions of actions and dependencies.
    While I'm here, format the files in the `examples/basic/` directory.

--- examples/basic/basic_test.py
+++ examples/basic/basic_test.py
@@ -4,10 +4,11 @@ import basic
 
 
 class TestBasic(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(basic.add(1, 2), 3)
-        self.assertEqual(basic.add(2, 2), 4)
+
+  def test_add(self):
+    self.assertEqual(basic.add(1, 2), 3)
+    self.assertEqual(basic.add(2, 2), 4)
 
 
 if __name__ == "__main__":
-    unittest.main()
+  unittest.main()
